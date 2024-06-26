3f114f60bb4e43d4aad5e4e111aa54a9e1adb6f6
gourav soni <gouravsoni8476@gmail.com>
2024-06-26 20:55:25 +0530

    done 7th chapter of dsa

---
+++ 7/Question.java
@@ -0,0 +1,40 @@
+/**
+ * Question
+ */
+public class Question {
+
+    public static void main(String[] args) {
+        // Q.1
+        // int x = 2, y = 5;
+
+        // int exp1 = (x * y / x);
+        // int exp2 = (x * (y / x));
+        // System.out.println(exp1 + " " + exp2);
+
+        // Q.2
+        // int x = 200, y = 50, z = 100;
+        // if (x > y && y > z) {
+        // System.out.println("Hello");
+        // }
+        // if (z > y && z < x) {
+        // System.out.println("Java");
+        // }
+        // if ((y + 200) < x && (y + 150) < z) {
+        // System.out.println("Hello Java");
+        // }
+
+        // Q.3
+        // int x, y, z;
+        // x = y = z = 2;
+        // x += y;
+        // y -= z;
+        // z /= (x + y);
+        // System.out.println(x + " " + y + " " + z);
+
+        // Q.4
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4 / 3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
+        System.out.println(exp);
+    }
+}
